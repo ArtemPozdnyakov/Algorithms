1c1536f28bae52c49177222649d078e8546fb5e0
Artem <artemedmshot@gmail.com>
2020-06-02 12:39:19 +0300

    Initial Commit

---
+++ Algorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithms
+//
+//  Created by admin on 6/2/20.
+//  Copyright © 2020 Artem Pozdnyakov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
